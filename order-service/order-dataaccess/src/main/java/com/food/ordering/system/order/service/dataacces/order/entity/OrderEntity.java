9cc4e10509426a0df0c5a126abd401543bdc2528
karol.kuberacki@gls-poland.com <karol.kuberacki@gls-poland.com>
2022-09-22 16:25:48 +0200

    Create Jpa repositrory and entity for order

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataacces/order/entity/OrderEntity.java
@@ -0,0 +1,62 @@
+package com.food.ordering.system.order.service.dataacces.order.entity;
+
+import com.food.ordering.system.domain.valueobject.OrderStatus;
+import com.food.ordering.system.order.service.domain.dto.create.OrderAddress;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+@Entity
+public class OrderEntity {
+
+    @Id
+    private UUID id;
+    private UUID customerId;
+    private UUID restaurantId;
+    private UUID tracingId;
+    private BigDecimal price;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    private String failureMessage;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private OrderAddressEntity orderAddress;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItemEntity> items;
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final OrderEntity that = (OrderEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
