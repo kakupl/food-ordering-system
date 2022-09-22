66322a40b58709b8b6837a78152de39f102906bc
karol.kuberacki@gls-poland.com <karol.kuberacki@gls-poland.com>
2022-09-22 10:35:32 +0200

    Create Kafka Producer

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException  {
+
+    public KafkaProducerException(final String message) {
+        super(message);
+    }
+}
